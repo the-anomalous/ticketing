9cf3a5b08306f348ad56e9d0a960b3adcbfb8683
anomalous <adityamusale4477@gmail.com>
2024-10-09 16:07:14 +0530

    add eslint config

--- tickets/.eslintrc.cjs
+++ tickets/.eslintrc.cjs
@@ -17,6 +17,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: './tsconfig.eslint.json',
+    tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
   rules: {
