0e395cf729e42378d44627847283657c4c9ccd39
anomalous <adityamusale4477@gmail.com>
2024-10-15 14:20:55 +0530

    add validateRequest middleware

--- tickets/src/routes/tickets.route.ts
+++ tickets/src/routes/tickets.route.ts
@@ -1,4 +1,6 @@
-import { Router } from 'express';
+import { requireAuth, validateRequest } from '@ad-tickets/commonlib';
+import { Request, Response, Router } from 'express';
+import { body } from 'express-validator';
 
 const router = Router();
 
@@ -6,8 +8,17 @@ router.get('/api/tickets/', (req, res) => {
   res.status(201).send('tickets');
 });
 
-router.post('/api/tickets/', (req, res) => {
-  res.status(201).send({});
-});
+router.post(
+  '/api/tickets/',
+  requireAuth,
+  [
+    body('title').notEmpty().withMessage('Title is required'),
+    body('price').isFloat({ gt: 0 }).withMessage('Invalid price'),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.status(201).send({});
+  }
+);
 
 export { router as createTicketRouter };
