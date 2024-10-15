5f15f690e98f2dda9e83871562f625183195829b
anomalous <adityamusale4477@gmail.com>
2024-10-15 14:30:40 +0530

    create tickets model

---
+++ tickets/src/models/tickets.model.ts
@@ -0,0 +1,65 @@
+import mongoose from 'mongoose';
+
+// interface for tickets attributes
+interface TicketsAttrs {
+  title: string;
+  price: number;
+  userId: string;
+}
+
+// interface for tickets document
+interface TicketsDoc extends mongoose.Document {
+  title: string;
+  price: number;
+  userId: string;
+}
+
+// interface for tickets Model
+interface TicketsModel extends mongoose.Model<TicketsDoc> {
+  build(attrs: TicketsAttrs): TicketsDoc;
+}
+
+const ticketsSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+// ticketsSchema.pre('save', async function (done) {
+//   if (this.isModified('password')) {
+//     const hashed = await PasswordManager.toHash(this.get('password'));
+//     this.set('password', hashed);
+//   }
+
+//   done();
+// });
+
+ticketsSchema.statics.build = (attrs: TicketsAttrs) => {
+  return new Tickets(attrs);
+};
+
+const Tickets = mongoose.model<TicketsDoc, TicketsModel>(
+  'Tickets',
+  ticketsSchema
+);
+
+export { Tickets };
